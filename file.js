a529f4631b8115ae77e15b00d299c932bebcf49a
Phakulty <anathonrichards23@gmail.com>
2025-07-02 05:09:39 +0100

    first commit

---
+++ file.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+// fs.readFile('./docs/blog.txt', (err, data) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(data.toString());
+// })
+
+// fs.writeFile('./docs/blog.txt', 'hello, oyioha', () => {
+//     console.log('file was written');
+// });
+
+// fs.writeFile('./docs/blog.txt1', 'hello, oyioha', () => {
+//     console.log('file was written');
+// });
+if (!fs.existsSync('./assets')){
+    fs.mkdir('./assets', (err) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log('folder created');
+});
+} else {
+    fs.rmdir('./assets', (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('folder deleted');
+    });
+}
+
+//delete file
+if (fs.existsSync('./docs/del.txt')) {
+    fs.unlink('./docs/del.txt', (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('file deleted');
+    });
+}
---
+++ people.js
@@ -0,0 +1,8 @@
+const people = ['Anthony', 'Bob', 'Charlie', 'David', 'Eve'];
+const ages = [25, 30, 35, 40, 45];
+
+console.log(people);
+
+
+module.exports = {
+    people, ages}
