5c8abef9b1bd613ac98d122c8603cf5a204e80a0
phakulty <anthonrichards23@gmail.com>
2025-07-12 04:32:55 +0100

    Add mongoose integration and blog model implementation

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+
 
 //express app
 const app = express();
 
+//connect to mongodb & listen for requests
+const dbURI = 'mongodb+srv://anthonymaky:23101997>node-js.mongodb.net/node-js';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => console.log('Connected to DB'))
+    .catch((err) => console.log(err));
+
 //reister view engine
 app.set('view engine', 'ejs');
 
-// listen for requests
+//mongoose and mongo sandbox routes
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'new blog ',
+        snippet: 'about my new blog',
+        body: 'more about my new blog'
+    });
 
-app.listen(3000);
+    blog.save()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
 
 //middleware & static files
-app.use(express.static('public'));
+app.use(express.static('public')); 
 app.use(morgan('dev'));
 
 app.get('/', (req, res) =>{
